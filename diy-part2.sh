e9383100c4ba46cdc5905e7ecd9ffd85956bac8b
ZarkZero <43511929+ZarkZero@users.noreply.github.com>
2022-05-27 05:30:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,6 +26,8 @@ sed -i s/10.10.10.1/192.168.1.1/g package/base-files/files/bin/config_generate
 
 #修改内核版本
 #sed -i s/5.15/5.10/g target/linux/ipq807x/Makefile
+sed -i s/110/115/g include/kernel-5.10.mk
+sed -i s/dbef6a06325433481551cb8cfca9254d908d0ae950bc809f3da8ade00c485693/f5e417b32f89318b6d0a230109a592ffd68997817463dc4692fa49ec7fe42f71/g include/kernel-5.10.mk
 
 #更换版本
 #rm -rf package/qca/nss/qca-ssdk
