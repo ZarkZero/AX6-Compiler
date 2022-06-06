5721b764b640ded56a5f9fff319966566b8995fa
ZarkZero <43511929+ZarkZero@users.noreply.github.com>
2022-06-06 22:48:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -34,7 +34,7 @@ sed -i s/10.10.10.1/192.168.1.1/g package/base-files/files/bin/config_generate
 
 
 #更换版本
-#rm -rf package/qca/nss/qca-ssdk
+rm -rf package/qca/nss/qca-ssdk
 #rm -rf package/qca/nss/qca-nss-dp
 #rm -rf package/qca/nss/qca-nss-clients
 #rm -rf package/qca/nss/qca-nss-ecm
