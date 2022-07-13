31da59e2d7e8d23c2749b5db601714a84007a0cd
ZarkZero <43511929+ZarkZero@users.noreply.github.com>
2022-07-13 23:42:14 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -27,6 +27,6 @@ rm -rf package/qca/nss/qca-nss-drv
 #添加额外非必须软件包
 git clone https://github.com/kiddin9/openwrt-packages.git package/openwrt-packages
 rm -rf package/openwrt-packages/firewall
-#rm -rf package/openwrt-packages/shortcut-fe
+rm -rf package/openwrt-packages/shortcut-fe
 svn co https://github.com/Boos4721/openwrt/trunk/package/firmware/ath11k-board package/openwrt-packages/ath11k-board
-#svn co https://github.com/Boos4721/openwrt/trunk/package/lean/shortcut-fe package/openwrt-packages/shortcut-fe
+svn co https://github.com/Boos4721/openwrt/trunk/package/lean/shortcut-fe package/openwrt-packages/shortcut-fe
