8c109788fc3ded6ad79358fd3bf080d55a56f5e6
ZarkZero <43511929+ZarkZero@users.noreply.github.com>
2022-07-13 23:48:54 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -3,9 +3,9 @@
 #添加额外非必须软件包
 git clone https://github.com/kiddin9/openwrt-packages.git package/openwrt-packages
 rm -rf package/openwrt-packages/firewall
-#rm -rf package/openwrt-packages/shortcut-fe
+rm -rf package/openwrt-packages/shortcut-fe
 svn co https://github.com/Boos4721/openwrt/trunk/package/firmware/ath11k-board package/openwrt-packages/ath11k-board
-#svn co https://github.com/Boos4721/openwrt/trunk/package/lean/shortcut-fe package/openwrt-packages/shortcut-fe
+svn co https://github.com/Boos4721/openwrt/trunk/package/lean/shortcut-fe package/openwrt-packages/shortcut-fe
 
 
 #Custom Package
