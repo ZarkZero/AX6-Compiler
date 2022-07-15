2fa523d8da083d34d80a38c5a5ffa81ee5e7e577
ZarkZero <43511929+ZarkZero@users.noreply.github.com>
2022-07-15 23:29:00 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -5,9 +5,9 @@ git clone https://github.com/kiddin9/openwrt-packages.git package/openwrt-packag
 rm -rf package/openwrt-packages/firewall
 rm -rf package/openwrt-packages/shortcut-fe
 svn co https://github.com/Boos4721/openwrt/trunk/package/firmware/ath11k-board package/openwrt-packages/ath11k-board
-svn co https://github.com/Boos4721/openwrt/trunk/package/lean/shortcut-fe package/openwrt-packages/shortcut-fe
-svn co https://github.com/Boos4721/openwrt/trunk/target/linux/generic/hack-5.15 hack-5.15
-cp -f hack-5.15/953-net-patch-linux-kernel-to-support-shortcut-fe.patch target/linux/generic/hack-5.15
+#svn co https://github.com/Boos4721/openwrt/trunk/package/lean/shortcut-fe package/openwrt-packages/shortcut-fe
+#svn co https://github.com/Boos4721/openwrt/trunk/target/linux/generic/hack-5.15 hack-5.15
+#cp -f hack-5.15/953-net-patch-linux-kernel-to-support-shortcut-fe.patch target/linux/generic/hack-5.15
 
 
 #Custom Package
